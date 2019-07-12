969baf70747bf28e2a4cd2cdc1754002e3f73c32
Vonetta Stevenson <vonettastevenson@gmail.com>
2019-07-11 22:34:19 -0400

    updating

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,33 +1,44 @@
-import axios from 'axios'
-
-export const createUser = async ({ firstName, lastName, email, password, verified, dateRegistered }) => {
-    console.log(dateRegistered)
-    try {
-        const response = await axios.post("http://localhost:3001/api/users/", {
-            firstName, lastName, email, password, verified, dateRegistered
-        })
-        return onSuccess(response)
-    }
-    catch (err) {
-        return onError(err)
-    }
-}
+import axios from "axios";
+const url = url;
+export const createUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+  verified,
+  dateRegistered
+}) => {
+  console.log(dateRegistered);
+  try {
+    const response = await axios.post(`${url}/api/users/`, {
+      firstName,
+      lastName,
+      email,
+      password,
+      verified,
+      dateRegistered
+    });
+    return onSuccess(response);
+  } catch (err) {
+    return onError(err);
+  }
+};
 
 export const loginUser = async ({ email, password }) => {
-    try {
-        const response = await axios.post("http://localhost:3001/api/users/login", {
-            email, password
-        })
-        return onSuccess(response.data)
-    }
-    catch (err) {
-        return onError(err)
-    }
-}
+  try {
+    const response = await axios.post(`${url}/api/users/login`, {
+      email,
+      password
+    });
+    return onSuccess(response.data);
+  } catch (err) {
+    return onError(err);
+  }
+};
 function onSuccess(response) {
-    return response
+  return response;
 }
 
 function onError(error) {
-    return Promise.reject(error.data)
+  return Promise.reject(error.data);
 }
